6bb8add6ae54e51cd07fd93fd6d6c8d63faa4034
calleerlandsson <calle@omnidev.se>
2013-11-03 19:47:49 +0100

    Added a file for manual testing

---
+++ test/test.rb
@@ -0,0 +1,12 @@
+require_relative '../lib/identiconify'
+
+string = ARGV[0]
+identicon = Identiconify::Identicon.new(string, size: 512)
+png_data = identicon.to_png_blob
+tmpdir = File.join(File.dirname(__FILE__), "tmp")
+Dir.mkdir(tmpdir) unless File.directory?(tmpdir)
+filename = File.join(tmpdir, "image.png")
+File.open(filename, "w") do |file|
+  file.write(png_data)
+end
+system "open #{filename}"
